0f2fbe1f751fac77ad47cb28bf06f494ba3119c1
JoaquinPenna <jpenna@fi.uba.ar>
2026-03-26 21:25:33 -0300

    TERMINADO, FIN, SE ACABO, por ahora

--- magios.c
+++ magios.c
@@ -27,15 +27,16 @@ const int AÑO_ACTUAL = 2026;
 const int AÑO_MINIMO = 1926;
 const int MES_ACTUAL = 3;
 const int PUNTOS_POR_EDAD = 2;
+const int EDAD_MINIMA = 18;
 
 const int LIMITES_FINALES[] = {0,151,251,350};
 const char RESULTADOS_FINALES[][MAXLINE] = { "-ASPIRANTE-", "-MAGIO NOVATO-", "-MAGIO-", "-LIDER SUPREMO-"};
-const int TOPE_RESULTADOS = 3;
+const int TOPE_RESULTADOS = 4;
 
-void imprimir_menu(int pregunta);
+void imprimir_menu(int pregunta); //
 bool es_respuesta_valida(int respuesta, int pregunta);
-bool fecha_valida( int año, int mes, char separador);
-int puntos_ganados(int pregunta, int intentos, int respuesta);
+bool fecha_valida( int año, int mes);
+int puntos_ganados(int pregunta, int respuesta);
 int calcular_edad(int año, int mes);
 void resultado_final(int puntos_totales);
 
@@ -54,7 +55,6 @@ int main(void){
 	char chartmp;
 	int puntos_2 = 0;
 
-	char separador = '\0';
 	int puntos_3 = 0;
 	int año = 0;
 	int mes = 0;
@@ -71,7 +71,7 @@ int main(void){
 		scanf(" %c",&respuesta_1);
 
 		if(respuesta_1 == OPCION_CORRECTA_1){
-			puntos_1 = puntos_ganados(pregunta, intentos, false);
+			puntos_1 = puntos_ganados(pregunta, false) - intentos * PUNTOS_PERDIDOS_POR_INTENTO;
 			respuesta_1_correcta = true;
 		}
 		else if( es_respuesta_valida(respuesta_1, pregunta) ){ 
@@ -105,7 +105,7 @@ int main(void){
 		}
 	}while( !es_respuesta_valida(chartmp, pregunta));
 	
-	puntos_2 = puntos_ganados(pregunta, 0, respuesta_2);
+	puntos_2 = puntos_ganados(pregunta, respuesta_2);
 	printf("%d puntos\n", puntos_2);
 	
 	pregunta++;
@@ -114,15 +114,15 @@ int main(void){
 //pregunta 3
 	do{
 		imprimir_menu(pregunta);
-		scanf("%d%c%d", &año, &separador, &mes);
-	}while( !fecha_valida(año, mes, separador) );
+		scanf("%d/%d", &año, &mes);
+	}while( !fecha_valida(año, mes) );
 
 	edad = calcular_edad(año, mes);
-	if (edad < 18){
+	if (edad < EDAD_MINIMA){
 		printf("-RECHAZADO-\n");
 		return 1;
 	}else{
-		puntos_3 = puntos_ganados(pregunta, 0, edad);
+		puntos_3 = puntos_ganados(pregunta, edad);
 		printf("%d puntos\n", puntos_3);
 	}
 	pregunta++;
@@ -134,14 +134,14 @@ int main(void){
 		scanf("%d", &respuesta_4);
 	}while( !(es_respuesta_valida(respuesta_4, pregunta)) );
 	
-	puntos_4 = puntos_ganados(pregunta,0,respuesta_4);
+	puntos_4 = puntos_ganados(pregunta, respuesta_4);
 	printf("%d puntos\n", puntos_4);
 	
 	pregunta++;
 
 
-//fin de las preguntas
-
+//fin de las preguntas, calculo del resultado final
+	//posible hacer que la funcion de puntos ganados reciba un pointer a un int donde luego de calcular los puntos ganados se sume a los puntos totales
 	puntos_total = puntos_1 + puntos_2 + puntos_3 + puntos_4;
 	printf("puntaje total = %d\nPregunta 1: %d\nPregunta 2: %d\nPregunta 3: %d\nPregunta 4: %d\n", puntos_total, puntos_1, puntos_2, puntos_3, puntos_4);
 
@@ -151,7 +151,8 @@ int main(void){
 
 }
 
-
+//PRE: se asume que la variable de "pregunta" se fue incrementando al ejecutarse el programa, y que esta estara entre 1 y 4,*/
+//POST: se imprime un mensaje de la pregunta con la informacion sobre el formato y las opciones que tiene el usuario
 void imprimir_menu(int pregunta){
 	switch(pregunta){
 		case 1:
@@ -161,10 +162,10 @@ void imprimir_menu(int pregunta){
 			}
 			break;
 		case 2:
-			printf("2. ¿Promete mantener en secreto la existencia de los Magios?\n\t[S] si\n\t[N] no\n"); // hacer que funcione con un forlup
+			printf("2. ¿Promete mantener en secreto la existencia de los Magios?\n\t[S] si\n\t[N] no\n");
 			break;
 		case 3:
-			printf("3. Fecha de nacimiento[yyyy/mm]: ");
+			printf("3.  ¿Cuál es su fecha de nacimiento? (formato: yyyy/mm)\n");
 			break;
 		case 4:
 			printf("4. ¿Cuántas donas estaría dispuesto a sacrificar para el Número Uno? [%d-%d]\n", MIN_SACRIFICIO, MAX_SACRIFICIO);
@@ -172,10 +173,16 @@ void imprimir_menu(int pregunta){
 	}
 }
 
-int puntos_ganados(int pregunta, int intentos, int respuesta){
+
+/*PRE: se asume que
+ * 	la variable pregunta es igual al numero de la pregunta la cual fue respondida por el usuario
+ * 	la respuesta del usuario fue validada y es la respuesta correcta si es que hay una
+ * el parametro de intentos es para poder calcular los puntos de la pregunta 1, como las demas preguntas no los utilizan, lo pasamos con 0*/
+//POST: se devuelve como un int la cantidad de puntos en base a la respuesta que se dio
+int puntos_ganados(int pregunta, int respuesta){
 	switch(pregunta){
 		case 1:
-			return PUNTOS_CORRECTA_1 - (intentos*PUNTOS_PERDIDOS_POR_INTENTO);
+			return PUNTOS_CORRECTA_1;
 			break;
 
 		case 2:
@@ -205,8 +212,12 @@ int puntos_ganados(int pregunta, int intentos, int respuesta){
 
 	}
 	return 0;
-}	
+}
 
+
+/*PRE: se asume que la variable pregunta es el numero de pregunta para el cual es la respuesta, y que la respuesta es del tipo solicitado en la prgunta
+ * excepto para la pregunta 3 la cual tiene su propia funcion */
+//POST: se devuelve true si el formato de la respuesta es valido y la respuesta esta en las opciones indicadas, y false si no
 bool es_respuesta_valida(int respuesta, int pregunta){
 	switch(pregunta){
 		case 1:
@@ -229,8 +240,12 @@ bool es_respuesta_valida(int respuesta, int pregunta){
 	return false;
 }
 
-bool fecha_valida( int año, int mes, char separador){
-	if( año < 0 || mes < 0 || separador != '/' ){
+
+//PRE: se esperan 2 ints, y un character de la forma %d%c%d
+//POST: se devuelve false si no se cumple con alguna de las condiciones para que la fecha de nacimiento sea valida, y true si no rompe ninguna regla
+bool fecha_valida( int año, int mes){
+	//hola profe, yo se que se ve feo pero podia hacerlo mucho mas feo
+	if( año < 0 || mes < 0 ){
 		printf("%s",  MENSAJE_RESPUESTA_INVALIDA);
 		return false;
 	}else if( año < 1000 || año > 9999 ){
@@ -249,6 +264,9 @@ bool fecha_valida( int año, int mes, char separador){
 	return true;
 }
 
+
+//PRE: recibe 2 ints validos
+//POST: devuelve un int el cual indica la edad de la persona nacida en esa fecha
 int calcular_edad(int año, int mes){
 	int edad = AÑO_ACTUAL - año;
 	if( mes < MES_ACTUAL ){
@@ -259,15 +277,18 @@ int calcular_edad(int año, int mes){
 }
 
 
+//PRE: se recibe la suma de todos los puntos como un entero
+//POST: se imprime el estado segun el arreglo RESILTADOS_FINALES 
 void resultado_final(int puntos){
+	printf("Tu estado es: ");
 	
 	int i = 0;
 	if ( puntos < LIMITES_FINALES[i] ){
 		printf("-RECHAZADO-");
-	}else if ( puntos >= LIMITES_FINALES[TOPE_RESULTADOS] ){
-		printf("%s",RESULTADOS_FINALES[TOPE_RESULTADOS]);
+	}else if ( puntos >= LIMITES_FINALES[TOPE_RESULTADOS-1] ){
+		printf("%s",RESULTADOS_FINALES[TOPE_RESULTADOS-1]);
 	}
-	while( i <= TOPE_RESULTADOS ){
+	while( i <= TOPE_RESULTADOS-1 ){
 		if( puntos >= LIMITES_FINALES[i] && puntos < LIMITES_FINALES[i+1] ){
 			printf("%s",RESULTADOS_FINALES[i]);
 		}
